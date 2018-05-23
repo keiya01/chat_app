433e42256c675861ff67b3a51e218a414004deee
sasaki keiya <keiyan.rad.yanke@gmail.com>
2018-05-24 07:26:10 +0900

    we could use session function in message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-	session = Thread.current[:request].session
+	session = Thread.current[:request].session ? Thread.current[:request].session : ""
 	after_create_commit { MessageBroadcastJob.perform_later(self, session[:user_id]) }
 	belongs_to :group
 	belongs_to :user

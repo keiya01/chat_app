e73b1db50ce7f03d53cc61f467f42df7f10f9b10
sasaki keiya <keiyan.rad.yanke@gmail.com>
2018-05-24 22:05:58 +0900

    edit room.channel.rb

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,16 +1,14 @@
 class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(message, user_id)
+  def perform(message, user)
   	group = message.group_id
-    RoomChannel.broadcast_to(group, message: render_message(message, user_id))
+    RoomChannel.broadcast_to(group, message: render_message(message, user))
   end
 
   private
-    def render_message(message, user_id)
-      message.user_id = user_id
-      message.save
-      user = User.find(user_id)
+    def render_message(message, user)
+      # user = User.find(user_id)
       ApplicationController.renderer.render(partial: 'messages/message', locals: { message: message, user: user })
     end
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
 	cattr_accessor :current
-	after_create_commit { MessageBroadcastJob.perform_later(self , Message.current.id) }
+	after_create_commit { MessageBroadcastJob.perform_later(self , Message.current) }
 	belongs_to :group
 	belongs_to :user
 end
